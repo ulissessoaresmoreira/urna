f43de9d07c72d882f1559a289ae647c0109ef278
Ulisses Soares <ulissessm@gmail.com>
2022-06-30 09:54:14 +0100

    adicionando estrutura da urna

---
+++ script.js
@@ -0,0 +1,17 @@
+function incluir(valor){
+    let valor1 = document.getElementById("campo1").value    
+    let valor2 = document.getElementById("campo2").value
+    
+    if(valor1 == ""){
+        document.getElementById("campo1").value = valor        
+    } else if(valor2 == ""){
+        document.getElementById("campo2").value = valor        
+    }
+}
+
+    
+
+
+
+
+
