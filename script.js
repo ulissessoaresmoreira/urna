366c8bc4004bc98f9c2e6be7880530ff12d42817
Ulisses Soares <ulissessm@gmail.com>
2022-06-30 12:12:43 +0100

    button corrige funcionando parcialmente

--- script.js
+++ script.js
@@ -7,9 +7,25 @@ function incluir(valor){
     } else if(valor2 == ""){
         document.getElementById("campo2").value = valor        
     }
+
 }
 
+function branco(){
+    console.log('VOTO EM BRANCO')
+}
+
+function corrige(){
+    let valor1 = document.getElementById("campo1").value
+    let valor2 = document.getElementById("campo2").value
     
+    if(valor1 != ""){
+        document.getElementById("campo1").value = ""        
+    } else if (valor2 != ""){
+        document.getElementById("campo1").value = ""
+        document.getElementById("campo2").value = ""        
+    }
+}
+
 
 
 
